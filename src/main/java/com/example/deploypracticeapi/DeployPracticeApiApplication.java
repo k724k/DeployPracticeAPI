388a825ea0b37c90895fb1b4e55fc0a252d45556
Yeon <yeon@yeongyeong.local>
2025-08-27 15:07:33 +0900

    Initial commit

---
+++ src/main/java/com/example/deploypracticeapi/DeployPracticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploypracticeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployPracticeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployPracticeApiApplication.class, args);
+    }
+
+}
