a9986e2eb0c56f7272463d313b2c15a0621850e4
연경 <k724k@naver.com>
2025-09-09 18:42:00 +0900

    fix : test.yml 생성

--- src/test/java/com/example/deploypracticeapi/ApplicationTests.java
+++ src/test/java/com/example/deploypracticeapi/ApplicationTests.java
@@ -2,9 +2,12 @@ package com.example.deploypracticeapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = Application.class)
-class ApplicationTests {
+@SpringBootTest
+@ActiveProfiles("test")
+public class ApplicationTests {
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }
