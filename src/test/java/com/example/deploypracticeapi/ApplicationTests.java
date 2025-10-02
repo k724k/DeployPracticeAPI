16b57a400687b008760fbb61a236cd0ff8d88bfb
연경 <k724k@naver.com>
2025-10-02 20:39:06 +0900

    fix : 테스트 클래스에 메인 클래스 명시

--- src/test/java/com/example/deploypracticeapi/ApplicationTests.java
+++ src/test/java/com/example/deploypracticeapi/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = ExampleApplication.class)
 @ActiveProfiles("test")
 public class ApplicationTests {
     @Test
