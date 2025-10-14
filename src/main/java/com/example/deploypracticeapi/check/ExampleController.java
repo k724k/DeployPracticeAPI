60441e57cc1501441a445d1cc3f7de2e16da778a
연경 <k724k@naver.com>
2025-10-14 15:52:06 +0900

    refactor : ExampleController url 변경

--- src/main/java/com/example/deploypracticeapi/check/ExampleController.java
+++ src/main/java/com/example/deploypracticeapi/check/ExampleController.java
@@ -2,14 +2,16 @@ package com.example.deploypracticeapi.check;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/check")
 public class ExampleController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World!";
+        return "Hello World!?";
     }
 
     @GetMapping("/health")
